793ea0130cfc2087a24a78f3c8050998f79694e7
Sarthak Suradkar <suradkarsarthak85@gmail.com>
2025-08-08 19:02:46 +0530

    Create motd.sh

---
+++ motd.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# motd.sh - Show Message of the Day in Jenkins output
+
+echo "======================================"
+echo " Welcome to $(hostname)"
+echo " Today is $(date)"
+echo " Have a great day!"
+echo "======================================"
